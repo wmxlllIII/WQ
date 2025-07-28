91ba054aa477c6293d558be31663b5fff4aa8a6e
wmx <W3051030071@163.com>
2025-07-29 00:19:32 +0800

    分页获取展示

---
+++ app/src/main/java/com/memory/wq/RecommendViewModel.java
@@ -0,0 +1,16 @@
+package com.memory.wq;
+
+import androidx.lifecycle.ViewModel;
+
+import com.memory.wq.beans.PostInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecommendViewModel extends ViewModel {
+    public List<PostInfo> postInfoList = new ArrayList<>();
+    public int currentPage = 1;
+    public boolean hasNextPage = true;
+    public boolean isLoading = false;
+    public List<Integer> bannerImageList = new ArrayList<>();
+}
