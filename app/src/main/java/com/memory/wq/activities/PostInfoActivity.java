46cd4ea4619f1f6333cbeedea976dca7e54f8f81
wmx <W3051030071@163.com>
2025-07-31 00:09:41 +0800

    分页获取展示

--- app/src/main/java/com/memory/wq/activities/PostInfoActivity.java
+++ app/src/main/java/com/memory/wq/activities/PostInfoActivity.java
@@ -1,20 +1,30 @@
 package com.memory.wq.activities;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.memory.wq.R;
 
-public class PostInfoActivity extends AppCompatActivity {
+public class PostInfoActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_info);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+        ImageView iv_back = (ImageView) findViewById(R.id.iv_back);
+        ImageView iv_avatar = (ImageView) findViewById(R.id.iv_avatar);
+        ImageView iv_share = (ImageView) findViewById(R.id.iv_share);
+        TextView tv_nickname = (TextView) findViewById(R.id.tv_nickname);
+        TextView tv_follow = (TextView) findViewById(R.id.tv_follow);
     }
 }
